8cb0c8fd781b5f2a3f1d21ffc3b5d186a981a114
rjv450 <65650339+rjv450@users.noreply.github.com>
2021-12-05 13:41:15 +0530

    first commit

---
+++ src/Component/NotFound.jsx
@@ -0,0 +1,9 @@
+import notfound from '../Assets/Images/pexels-lukas-669615.jpg';
+
+const NotFound = () => {
+    return (
+        <img src={notfound}  style={{width: '100%'}}/>
+    )
+}
+
+export default NotFound;
